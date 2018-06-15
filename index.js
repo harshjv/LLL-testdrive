f341a60b2983c355d6fc7b2b220f715f6116dca4
Harsh Vakharia <harshjv@gmail.com>
2018-06-15 16:31:33 +0530

    add references

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ Promise.promisifyAll(web3.eth)
 
 let lllContractData = fs.readFileSync('./build/ETHSwap.lll.bin', 'utf8')
 let solContractData = fs.readFileSync('./build/ETHSwap.bin', 'utf8')
+// let tingContractData = `602060a5600039600051600055602060c5600039600051600155602060e560003960005160025533600355606e8060006037396000f3006000341160235760e060020a60003504806379599f961460245763bd66528a14603e57fe5b60015442106023576000808080303160035460155a03f150005b600254803314156023576004356000526032600020806000541415602357600052600080808030319460155a035000`
+let tingContractData = `6020609d600039600051600055602060bd600039600051600155602060dd6000396000516002553360035560668060006037396000f3006000341160235760e060020a60003504806379599f961460245763bd66528a14603457fe5b6001544210602357600354605656005b6002548033141560235760043560005260326000208060005414156023576000525b600080808030318560155a03f1500`
 
 lllContractData = lllContractData.trim()
 solContractData = solContractData.trim()
 
 const lcl = lllContractData.length
 const scl = solContractData.length
+const tcl = tingContractData.length
 
-console.log(`LLL produced ${(Math.round((scl/lcl) * 100)/100)}x smaller bytecode`)
+console.log(` SOL produced ${scl/2} byte code`)
+console.log(` LLL produced ${lcl/2} byte code. ${(Math.round((scl/lcl) * 100)/100)}x reduction`)
+console.log(`Ting produced ${tcl/2} byte code. ${(Math.round((scl/tcl) * 100)/100)}x reduction`)
 console.log('')
 
 const abi = require('./build/ETHSwap')
@@ -43,72 +48,49 @@ function NewETHSwap (hash, lock, to, from, value, data) {
   });
 }
 
-const STANDARDTXGAS = 21000
-
 async function run () {
   const accounts = await web3.eth.getAccountsAsync()
 
   const value = 2e18
-  const from = accounts[4]
-  const to  = accounts[5]
-
-  console.log(`Value: ${value} wei`);
-  console.log(`From: ${from}`)
-  console.log(`To: ${to}`)
-
-  console.log('')
-
-  console.log('Account F:', (await web3.eth.getBalanceAsync(from)).toString())
-  console.log('Account T:', (await web3.eth.getBalanceAsync(to)).toString())
-  console.log('')
+  const from = accounts[8]
+  const to  = accounts[9]
 
   const args = [ '0x3943fb730471b38900768f17e0ed5bbb48725a380b2fedb519bc29a829589ee1', 123, to ]
 
   const lllContract = await NewETHSwap(...args, from, value, lllContractData)
   const solContract = await NewETHSwap(...args, from, value, solContractData)
-
-  // console.log(lllContract.address)
-  // console.log(solContract.address)
+  const tingContract = await NewETHSwap(...args, from, value, tingContractData)
 
   Promise.promisifyAll(lllContract)
   Promise.promisifyAll(solContract)
+  Promise.promisifyAll(tingContract)
 
-  const creationGasUsedLLL = await getGasUsed(lllContract.transactionHash)
   const creationGasUsedSol = await getGasUsed(solContract.transactionHash)
+  const creationGasUsedLLL = await getGasUsed(lllContract.transactionHash)
+  const creationGasUsedTing = await getGasUsed(tingContract.transactionHash)
 
-  console.log(`LLL> Gas used to deploy contract: ${creationGasUsedLLL}, ${creationGasUsedLLL - STANDARDTXGAS} more`)
-  console.log(`SOL> Gas used to deploy contract: ${creationGasUsedSol}, ${creationGasUsedSol - STANDARDTXGAS} more`)
-  console.log(`>>>> Savings: ${creationGasUsedSol - creationGasUsedLLL}`)
+  let STANDARDTXGAS = creationGasUsedSol
+
+  console.log(` SOL> Gas used to deploy contract: ${creationGasUsedSol}, reference`)
+  console.log(` LLL> Gas used to deploy contract: ${creationGasUsedLLL}, ${STANDARDTXGAS - creationGasUsedLLL} less`)
+  console.log(`TING> Gas used to deploy contract: ${creationGasUsedTing}, ${STANDARDTXGAS - creationGasUsedTing} less`)
   console.log('')
 
   const claimArgs = [ '0x8ae25d6e387af39ed76da2422c00547089be2890d1f4882e630da8f672ccbb1d', { from: to } ]
 
-  // ////
-  //
-  // const transaction = {
-  //   from: to,
-  //   to: lllContract.address,
-  //   value: '0x00',
-  //   gas: 5000000,
-  //   data: '0xc63ff8dd0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000568656c6c6f000000000000000000000000000000000000000000000000000000'
-  // }
-  //
-  // const temp = await web3.eth.sendTransactionAsync(transaction)
-  //
-  // console.log(temp)
-
-  ////
-
   const txLLL = await lllContract.claimAsync(...claimArgs)
   const txSol = await solContract.claimAsync(...claimArgs)
+  const txTing = await tingContract.claimAsync(...claimArgs)
 
-  const claimGasUsedLLL = await getGasUsed(txLLL)
   const claimGasUsedSol = await getGasUsed(txSol)
+  const claimGasUsedLLL = await getGasUsed(txLLL)
+  const claimGasUsedTing = await getGasUsed(txTing)
 
-  console.log(`LLL> Gas used to claim the swap: ${claimGasUsedLLL}, ${claimGasUsedLLL - STANDARDTXGAS} more`)
-  console.log(`SOL> Gas used to claim the swap: ${claimGasUsedSol}, ${claimGasUsedSol - STANDARDTXGAS} more`)
-  console.log(`>>>> Savings: ${claimGasUsedSol - claimGasUsedLLL}`)
-  console.log('')
+  STANDARDTXGAS = claimGasUsedSol
+
+  console.log(` SOL> Gas used to claim the swap: ${claimGasUsedSol}, reference`)
+  console.log(` LLL> Gas used to claim the swap: ${claimGasUsedLLL}, ${STANDARDTXGAS - claimGasUsedLLL} less`)
+  console.log(`TING> Gas used to claim the swap: ${claimGasUsedTing}, ${STANDARDTXGAS - claimGasUsedTing} less`)
 }
 
 run()
